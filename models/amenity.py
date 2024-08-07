4034bd621b64cd6ee83f4c77ee0a57a03db107be
FarieB <faraibande@gmail.com>
2024-08-07 06:17:34 -0700

    The Console

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This module creates a Amenity class"""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class for managing amenity objects"""
+
+    name = ""
+
--- web_static/README.md
+++ web_static/README.md
@@ -1,1 +1,2 @@
-0x01. AirBnB clone - Web static
+Project web static
+
